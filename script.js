1dde06e83e6c6813f4aefa654353994dd8a3fec9
Diogo <Ninjaspedd123@hotmail.com>
2025-02-28 22:13:16 -0300

    ajustes

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".progress-circle").forEach(circle => {
-        let value = circle.getAttribute("data-progress");
-        circle.style.background = `conic-gradient(#00d2ff ${value}%, #ddd ${value}%)`;
-        circle.querySelector("span").textContent = `${value}%`;
-    });
-});
